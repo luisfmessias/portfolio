d3c398265604863a5ea7cfde339af7461c5d1b67
luis <luisfernandosilva8855@gmail.com>
2026-01-07 14:20:38 -0300

    fix: case sensitive

--- src/components/Contato.tsx
+++ src/components/Contato.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react' 
 import { motion } from 'framer-motion'
 import wpp from '../public/AssetsPortifolio/Contato/whatsapp.png'
 import insta from '../public/AssetsPortifolio/Contato/social-media.png'
