bb8756e43193d8a1ab32ab77b6e558931cf0a8d7
luis <luisfernandosilva8855@gmail.com>
2026-02-25 21:54:27 -0300

    fix: vercel build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const isVercel = !!process.env.VERCEL
-
 export default defineConfig({
-  base: isVercel ? '/' : '/portfolio/',
+  base: '/',
   plugins: [react()],
-  build: {
-    outDir: 'docs',
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
       '@': '/src',
